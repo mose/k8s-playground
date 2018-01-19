35b3b66eabde4fbe3b27d3f5e8f23c68fa169356
mose <mose@mose.com>
2018-01-19 17:53:02 +0800

    uniform indentation

--- kubeless/stargazers.rb
+++ kubeless/stargazers.rb
@@ -5,23 +5,23 @@ require "logger"
 
 def handler(request)
   webhook_url = ENV['WEBHOOK_URL']
-	payload = JSON.parse(request.body.read)
-	repo = payload['repository']['name']
-	stars = payload['repository']['stargazers_count']
-	username = payload['sender']['login']
-	url = payload['sender']['html_url']
-	text = "New Github star for *#{repo}* repo! It now has *#{stars}* stars! :tada:
+  payload = JSON.parse(request.body.read)
+  repo = payload['repository']['name']
+  stars = payload['repository']['stargazers_count']
+  username = payload['sender']['login']
+  url = payload['sender']['html_url']
+  text = "New Github star for *#{repo}* repo! It now has *#{stars}* stars! :tada:
 Your new fan is <#{url}|#{username}>"
-	uri = URI.parse(webhook_url)
-	http = Net::HTTP.new(uri.host, 443)
+  uri = URI.parse(webhook_url)
+  http = Net::HTTP.new(uri.host, 443)
   http.use_ssl = true
 
-	request = Net::HTTP::Post.new(
-		uri.request_uri,
-		'Content-Type': 'application/json'
-	)
-	request.body = { 'text': text }.to_json
-	response = http.request(request)
-	logger = Logger.new(STDOUT)
-	logger.info("Slack posted, reponded #{response.code}.")
+  request = Net::HTTP::Post.new(
+    uri.request_uri,
+    'Content-Type': 'application/json'
+  )
+  request.body = { 'text': text }.to_json
+  response = http.request(request)
+  logger = Logger.new(STDOUT)
+  logger.info("Slack posted, reponded #{response.code}.")
 end
